fc44ed09e67dd8b370e70898dca9dcdef8d0066e
DavidIlnytskyi <ilnytskyy.d@gmail.com>
2025-03-19 16:22:53 +0200

    Add python scripts

--- python-scripts/logging_service.py
+++ python-scripts/logging_service.py
@@ -5,30 +5,20 @@ from domain import *
 import sys
 
 app = FastAPI()
-messages = dict()
+distributed_map = None
 
 @app.post("/")
 def add_data(data: DataModel):
-    distributed_map = client.get_map("my-distributed-map").blocking()
-
-    if data.uuid in messages.keys():
-        print(f"UUID Duplication for msg: {data.msg}")
-        return {"msg": "duplication"}
-
-    messages[data.uuid] = data.msg
-
     distributed_map.set(data.uuid, data.msg)
 
     return {"msg": "success"}
 
 @app.get("/")
 def get_data():
-    distributed_map = client.get_map("my-distributed-map").blocking()
 
     messages = distributed_map.entry_set()
 
     return {"messages": messages}
-
 
 if __name__ == "__main__":
     host_url = sys.argv[1]
@@ -39,5 +29,7 @@ if __name__ == "__main__":
             hazelcast_url,
         ]
     )
+
+    distributed_map = client.get_map("my-distributed-map").blocking()
 
     uvicorn.run(app, host=host_url[:-5], port=int(host_url[-4:]))
--- python-scripts/setup.py
+++ python-scripts/setup.py
@@ -7,11 +7,11 @@ config = configparser.ConfigParser()
 
 config.read('./ips.ini')
 
-subprocess.run(["python3", "facade_service.py", 
+subprocess.run(["python3", "./python-scripts/facade_service.py", 
                 config["DEFAULT"]["Facade-Service"], 
                 config["DEFAULT"]["Logging-Service-0"],
                 config["DEFAULT"]["Messages-Service"]
                 ])
 
-for idx in range(NUMBER_NODES):
-    subprocess.run(["python3", "logging_service.py", config["DEFAULT"][f"Logging-Service-{idx}"], config["DEFAULT"][f"Hazelcast-Node-{idx}"]])
+# for idx in range(NUMBER_NODES):
+#     subprocess.run(["python3", "logging_service.py", config["DEFAULT"][f"Logging-Service-{idx}"], config["DEFAULT"][f"Hazelcast-Node-{idx}"]])
